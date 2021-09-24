2ea902ebc0f6ec7d40ee6166b40d47066a147cac
Mickael Guesnon <mickael.guesnon@3rein.com>
2021-09-24 15:39:00 -0400

    Go fmt

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/pvotal-tech/terraform-provider-data-utils/internal/provider"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/pvotal-tech/terraform-provider-data-utils/internal/provider"
 )
 
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
